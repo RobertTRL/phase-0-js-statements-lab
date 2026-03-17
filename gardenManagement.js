2b4ccb899b61b5268093ac155136a51551519442
Robert Toroitich <robertojnr2802@gmail.com>
2026-03-17 17:30:04 +0300

    Initial commit

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,13 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+(temperature > 80) ? console.log("Watering on") : console.log("Watering off");
+(timeOfDay === "evening" || timeOfDay === "night") ? console.log("Lights on") : console.log("Lights off");
+
+while (soilMoisture < 40) {
+    console.log(soilMoisture)
+    soilMoisture += 5
+}
+console.log(soilMoisture)
+
